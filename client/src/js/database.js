19878405022366f5e3223ddaf2ac5b5bf818d8a3
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-07-18 09:38:01 -0400

    Fixing error not getting any data from getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,7 +27,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
-  // return result.value;
+  return (result ? result.value : undefined)
 };
 
 initdb();
